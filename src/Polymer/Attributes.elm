308dd8bbb8ae19b9aecdf8e69681a5628165192f
Tadeusz Łazurski <tadeusz@lazurski.pl>
2017-07-03 22:44:43 +0200

    Add docs to Polymer.Attributes module
    Via template

--- src/Polymer/Attributes.elm
+++ src/Polymer/Attributes.elm
@@ -7,7 +7,12 @@ module Polymer.Attributes
 
 {-| Common types of attributes.
 
-Used in */Attributes.eml modules
+Used in `Polyner.*.Attributes` modules. You hopefully won't have to use them directly in your application, but they are exposed just in case.
+
+
+# Helpers
+
+@docs bool, number, string
 
 -}
 
@@ -16,6 +21,35 @@ import Html.Attributes exposing (attribute, property)
 import Json.Encode
 
 
+{-| Helper for an attribute that will take a boolean value.
+
+Note that if the value is false, the attribute won't be added to the element, e.g.:
+
+    drawer
+        [ bool "opened" False
+        ]
+        []
+
+Will yield following DOM Element:
+
+```html
+<app-drawer></app-drawer>
+```
+
+whereas
+
+    drawer
+        [ bool "opened" True
+        ]
+        []
+
+will yield:
+
+```html
+<app-drawer opened></app-drawer>
+```
+
+-}
 bool : String -> Bool -> Attribute msg
 bool name value =
     value
@@ -23,11 +57,24 @@ bool name value =
         |> property name
 
 
+{-| This is just an alias for `Html.Attributes.attribute`
+-}
 string : String -> String -> Attribute msg
 string =
     attribute
 
 
+{-| Helper for an attribute that will take a numerical value.
+
+    progress [ number "value" 99 ] []
+
+will yield:
+
+```html
+<paper-progress value="99"></paper-progress>
+```
+
+-}
 number : String -> Float -> Attribute msg
 number name value =
     value
